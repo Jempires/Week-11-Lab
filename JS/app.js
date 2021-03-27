7e3d66ed9e14c38ef36847636e5b39287d375c0c
Jennifer <Rafter.jennifer@gmail.com>
2021-03-26 22:33:01 -0700

    todoList array of 4 objects and empty array for completedTodos created

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,8 @@
+let todoList = [
+    {todo:"clone repo for todo list", status:"complete"},
+    {todo:"create 4 objects for array", status:"complete"},
+    {todo:"create a for loop to go over completed todo", status:"started"},
+    {todo:"push code to my own github repo", status:"started"}
+]
 
-const todoitem1 = {todo:"clone repo for todo list", status:"complete"};
-const todoitem2 = {todo:"create 4 objects for array", status:"complete"};
-const todoitem3 = {todo:"create a for loop to go over completed todo", status:"started"};
-const todoitem4 = {todo:"push code to my own github repo", status:"started"};
+let completedTodos = []
