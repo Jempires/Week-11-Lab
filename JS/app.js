7d8665662131aa5a45685f6181f066c4823ff63c
Jennifer <Rafter.jennifer@gmail.com>
2021-03-26 22:18:40 -0700

    Added links to css file and js in index created 4 objects in js file for todo list

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,5 @@
-// your JavaScript goes here, you can delete this comment
+
+const todoitem1 = {todo:"clone repo for todo list", status:"complete"};
+const todoitem2 = {todo:"create 4 objects for array", status:"complete"};
+const todoitem3 = {todo:"create a for loop to go over completed todo", status:"started"};
+const todoitem4 = {todo:"push code to my own github repo", status:"started"};
