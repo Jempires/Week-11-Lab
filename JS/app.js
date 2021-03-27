117a8ff58963e6b94c962099b38f8699bdadc577
Jennifer <Rafter.jennifer@gmail.com>
2021-03-27 01:20:46 -0700

    Finished First for Loop to Check Complete todos

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,19 @@
 let todoList = [
-    {todo:"clone repo for todo list", status:"complete"},
-    {todo:"create 4 objects for array", status:"complete"},
-    {todo:"create a for loop to go over completed todo", status:"started"},
-    {todo:"push code to my own github repo", status:"started"}
+    {todo: "clone repo for todo list", status:"complete"},
+    {todo: "create 4 objects for array", status:"complete"},
+    {todo: "create a for loop to go over todo", status:"started"},
+    {todo: "push code to my own github repo", status:"started"}
 ]
 
 let completedTodos = []
+
+var todoLength = todoList.length
+var i = 0
+
+for (todoList[i]; i < todoLength; i++) {
+    if (todoList[i].status == "complete") {
+        completedTodos.push(todoList[i])
+    }
+}
+
+console.log(completedTodos)
