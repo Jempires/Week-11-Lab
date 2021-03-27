42bfef99845be96a9ca9bd1884af482cc1a556fc
Jennifer <Rafter.jennifer@gmail.com>
2021-03-27 01:34:10 -0700

    Second for loop complete, logs to console

--- JS/app.js
+++ JS/app.js
@@ -1,19 +1,24 @@
 let todoList = [
-    {todo: "clone repo for todo list", status:"complete"},
-    {todo: "create 4 objects for array", status:"complete"},
-    {todo: "create a for loop to go over todo", status:"started"},
-    {todo: "push code to my own github repo", status:"started"}
+    {todo: 'clone repo for todo list', status:'complete'},
+    {todo: 'create 4 objects for array', status:'complete'},
+    {todo: 'create a for loop to go over todo', status:'started'},
+    {todo: 'push code to my own github repo', status:'started'}
 ]
 
-let completedTodos = []
+var completedTodos = []
 
 var todoLength = todoList.length
 var i = 0
-
 for (todoList[i]; i < todoLength; i++) {
-    if (todoList[i].status == "complete") {
+    if (todoList[i].status == 'complete') {
         completedTodos.push(todoList[i])
     }
 }
 
-console.log(completedTodos)
+var completedLength = completedTodos.length
+var j = 0
+for (completedTodos[j]; j < completedLength; j++) {
+    if (completedTodos[j].status == 'complete') {
+        console.log(completedTodos[j].status)
+    }
+}
